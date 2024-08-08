b900d6b794862a046b168afca8485083d1b1e616
yuvaraj-k123 <yuvaraj241364@gmail.com>
2024-08-08 19:34:37 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,5 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    
+
+    protected $table ='products';
+
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'description',
+        'price',
+        'image_path',
+        'qty',
+        'alert_stock',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(brand::class);
+    }
+
+    public function getImagePath()
+    {
+        return env('DOMAIN_URL') . Storage::url($this->image_path);
+    }
 }
