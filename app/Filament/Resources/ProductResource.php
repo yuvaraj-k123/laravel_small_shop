9a624813dfcdc373d3b8e652fe06b51a02a9da52
yuvaraj-k123 <yuvaraj241364@gmail.com>
2024-08-08 19:38:15 +0530

    Update ProductResource.php

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -23,7 +23,30 @@ class ProductResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('category_id')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\TextInput::make('brand_id')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('price')
+                    ->maxLength(10)
+                    ->default(null),
+                Forms\Components\TextInput::make('description')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\FileUpload::make('image_path')
+                    ->image(),
+                Forms\Components\TextInput::make('qty')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('alert_stock')
+                    ->required()
+                    ->maxLength(255)
+                    ->default(0),
             ]);
     }
 
@@ -31,7 +54,31 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('category_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('brand_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image_path'),
+                Tables\Columns\TextColumn::make('qty')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('alert_stock')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
