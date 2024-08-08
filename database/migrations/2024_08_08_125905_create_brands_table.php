1cd4a7ebdf42718a74e5e09d1733862e390cefcf
yuvaraj-k123 <yuvaraj241364@gmail.com>
2024-08-08 19:12:44 +0530

    Update 2024_08_08_125905_create_brands_table.php

--- database/migrations/2024_08_08_125905_create_brands_table.php
+++ database/migrations/2024_08_08_125905_create_brands_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('image_path')->nullable();
+            $table->string('image_path')->default("no-image-available.jpg")->nullable();
 
             $table->timestamps();
         });
